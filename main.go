8d57e9a0c24c727331c8e3aae19a574dc50904b5
bluespada <pentingmain@gmail.com>
2022-11-13 02:19:49 +0800

    feat : initialize database and http server

--- main.go
+++ main.go
@@ -8,9 +8,15 @@ file COPYING or http://www.opensource.org/licenses/mit-license
 package main
 
 import (
+	"log"
+	"os"
 	"sync"
 
+	ServerUtils "github.com/bluespada/megram/src/utils"
+	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func main() {
@@ -18,6 +24,32 @@ func main() {
 	godotenv.Load()
 
 	var wg = new(sync.WaitGroup)
+	app := fiber.New(fiber.Config{
+		AppName: "MeGram Server",
+	})
+
+	wg.Add(2)
+
+	go func() {
+		// initialize database
+		db, err := gorm.Open(postgres.New(postgres.Config{
+			DSN:                  os.Getenv("DB_DSN"),
+			PreferSimpleProtocol: true,
+		}))
+		if err != nil {
+			log.Fatalln(err)
+		}
+		// initialize auto migrate for models
+
+		ServerUtils.AppDatabase = db
+		wg.Done()
+	}()
+
+	go func() {
+		// initialize server
+		log.Fatalln(app.Listen(":" + os.Getenv("APP_PORT")))
+		wg.Done()
+	}()
 
 	// wait
 	wg.Wait()
